86e22b1a45e4277422508c4384ea228c4df6e266
GabrielHedric <gabriel.hedric@gmail.com>
2023-03-05 21:06:57 -0300

    commit final challenge

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -2,57 +2,29 @@ package br.com.dio.desafio.dominio;
 
 import java.util.Objects;
 
-public class Curso {
+public class Curso extends Conteudo {
 
-    private String titulo;
-    private String descricao;
-    private Double cargaHoraria;
+    private int cargaHoraria;
 
-    public Curso(String titulo, String descricao, Double cargaHoraria) {
-        this.titulo = titulo;
-        this.descricao = descricao;
-        this.cargaHoraria = cargaHoraria;
+    public Curso(){
+
     }
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public Double getCargaHoraria() {
+    public int getCargaHoraria() {
         return cargaHoraria;
     }
 
-    public void setCargaHoraria(Double cargaHoraria) {
+    public void setCargaHoraria(int cargaHoraria) {
         this.cargaHoraria = cargaHoraria;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Curso curso = (Curso) o;
-        return Objects.equals(titulo, curso.titulo) && Objects.equals(descricao, curso.descricao) && Objects.equals(cargaHoraria, curso.cargaHoraria);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(titulo, descricao, cargaHoraria);
     }
 
     @Override
     public String toString() {
-        return "Titulo: " + this.titulo + ", Descrição: " + this.descricao + ", Carga Horária: " + this.cargaHoraria;
+        return "Nome: " + this.getTitulo() + ", Descrição: " + this.getDescricao() + ", Carga Horária: " + this.cargaHoraria;
+    }
+
+    @Override
+    public double calcularExperiencia() {
+        return XP_PADRAO * cargaHoraria;
     }
 }
